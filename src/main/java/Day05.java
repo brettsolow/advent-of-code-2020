2e907afb8b4b77ac8153e6d88b4e6f2908af2ba5
Brett Solow <bsolow@squarespace.com>
2020-12-05 17:55:09 -0800

    Optimize day 5

--- src/main/java/Day05.java
+++ src/main/java/Day05.java
@@ -1,5 +1,7 @@
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class Day05 implements Day {
 
@@ -11,54 +13,26 @@ public class Day05 implements Day {
   @Override
   public String part1(List<String> lines) {
     int maxSeatId = lines.stream()
-        .map(Seat::fromBoardingPass)
-        .mapToInt(Seat::getSeatId)
+        .mapToInt(Day05::passToInt)
         .max().orElseThrow(NoSuchElementException::new);
     return String.valueOf(maxSeatId);
   }
 
   @Override
   public String part2(List<String> lines) {
-    int[] seatsInList = lines.stream()
-        .mapToInt(Seat::passToInt)
-        .sorted()
-        .toArray();
-    for (int i = 0; i < seatsInList.length; i++) {
-      if (seatsInList[i] - i != seatsInList[0]) {
-        int missingSeatInt = seatsInList[i] - 1;
-        Seat missingSeat = Seat.fromInt(missingSeatInt);
-        return String.valueOf(missingSeat.getSeatId());
-      }
-    }
-    throw new NoSuchElementException("seat not found");
+    IntSummaryStatistics stats = lines.stream()
+        .collect(Collectors.summarizingInt(Day05::passToInt));
+    // sum of arithmetic series
+    long expectedTotal = (stats.getCount() + 1) * (stats.getMin() + stats.getMax()) / 2;
+    long missingSeatId = expectedTotal - stats.getSum();
+    return String.valueOf(missingSeatId);
   }
 
-  static class Seat {
-
-    int row;
-    int col;
-
-    static Seat fromInt(int n) {
-      Seat seat = new Seat();
-      seat.row = n >> 3;
-      seat.col = n & 7;
-      return seat;
-    }
-
-    static Seat fromBoardingPass(String boardingPass) {
-      return fromInt(passToInt(boardingPass));
-    }
-
-    private static int passToInt(String boardingPass) {
-      String binaryStr = boardingPass.replace("B", "1")
-          .replace("F", "0")
-          .replace("R", "1")
-          .replace("L", "0");
-      return Integer.parseInt(binaryStr, 2);
-    }
-
-    private int getSeatId() {
-      return row * 8 + col;
-    }
+  private static int passToInt(String pass) {
+    String binaryStr = pass.replace("B", "1")
+        .replace("F", "0")
+        .replace("R", "1")
+        .replace("L", "0");
+    return Integer.parseInt(binaryStr, 2);
   }
 }
